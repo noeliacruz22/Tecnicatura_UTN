9cb33b3772eb071730996354cc3bab3fb9921b19
Noelia Cruz <china2.2@hotmail.com>
2024-11-09 15:59:37 -0300

    PTF/Capitulo9y10: creamos el componente Intereses, el Fondo Lava y el Footer

--- SegundoSemestre/Portafolio_Vuejs/src/App.vue
+++ SegundoSemestre/Portafolio_Vuejs/src/App.vue
@@ -1,14 +1,15 @@
 <script setup>
-import NavBar from './components/icons/NavBar.vue'; 
-import DatosPersonales from './components/icons/DatosPersonales.vue'; 
-import EducacionComp from './components/icons/EducacionComp.vue';
+import NavBar from './components/NavBar.vue'; 
+import DatosPersonales from './components/DatosPersonales.vue'; 
+import EducacionComp from './components/EducacionComp.vue';
 import ExperienciaComp from './components/ExperienciaComp.vue';
 import ProyectosComp from './components/ProyectosComp.vue';
 import HabilidadesComp from './components/HabilidadesComp.vue';
+import InteresesComp from './components/InteresesComp.vue';
 </script>
 
 <template>
-  <header>
+  <header id="top">
     <!--barra de navegacion-->
     <NavBar />
     <!--Datos Personales-->
@@ -38,17 +39,26 @@ import HabilidadesComp from './components/HabilidadesComp.vue';
         <section id="intereses">
             <!--Componente Intereses-->
             <h2>Intereses</h2>
-            <InteresesComponente />
+            <InteresesComp />
         </section>
     </main>
 
   </header>
-
+  <footer>
+        <a href="#top">Inicio</a>
+        <p>© 2024 Noelia Cruz - Portafolio web ♥ - tutorial UTN FRSR</p>
+  </footer>
 
 </template>
 
 <style scoped>
-
+footer {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 2rem;
+    font-size: 1.3rem;
+}
 
 
 </style>
