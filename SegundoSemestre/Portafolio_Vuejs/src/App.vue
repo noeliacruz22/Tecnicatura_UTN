8fadc2050cf4f717a0706114d18f48cd63c17af7
Noelia Cruz <china2.2@hotmail.com>
2024-11-09 11:10:09 -0300

    PTF/Capitulo3: Barra de Navegacion

---
+++ SegundoSemestre/Portafolio_Vuejs/src/App.vue
@@ -0,0 +1,46 @@
+<script setup>
+import NavBar from './components/icons/NavBar.vue'; 
+</script>
+
+<template>
+  <header>
+    <!--barra de navegacion-->
+    <NavBar />
+    <main>
+        <section id="educacion">
+          <h2>Educación - Cursos</h2>
+          <!--Componente Educación-->
+          <Educacion />
+        </section>
+        <section id="experiencia">
+            <h2>Experiencia</h2>
+            <!--Componente Experiencia-->
+            <ExperienciaComponente />
+        </section>
+        <section id="proyectos">
+            <h2>Proyectos</h2>
+            <!--Componente Proyectos-->
+            <ProyectosComponente />
+        </section>
+        <section id="habilidades">
+            <!--Componente Habilidades-->
+            <h2>Habilidades</h2>
+            <HabilidadesComponente />
+        </section>
+        <section id="intereses">
+            <!--Componente Intereses-->
+            <h2>Intereses</h2>
+            <InteresesComponente />
+        </section>
+    </main>
+
+  </header>
+
+
+</template>
+
+<style scoped>
+
+
+
+</style>
