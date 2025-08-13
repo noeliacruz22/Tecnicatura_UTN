42c455b3217a517eabd189f9540c15ce330c35f5
Noelia Cruz <china2.2@hotmail.com>
2025-08-13 20:56:09 -0300

    Iniciando el cuarto semestre

---
+++ CuartoSemestre/Javascript/E-commerce/client/js/products.js
@@ -0,0 +1,44 @@
+const productos = [
+    {
+        id: 1,
+        productName: "frutilla",
+        price: 480,
+        quanty: 1,
+        img: "/client/media/frutilla.png",
+    },
+    {
+        id: 2,
+        productName: "leche",
+        price: 950,
+        quanty: 1,
+        img: "/client/media/leche.webp",
+    },
+    {
+        id: 3,
+        productName: "lechuga",
+        price: 750,
+        quanty: 1,
+        img: "/client/media/lechuga.jpg",
+    },  
+    {
+        id: 4,
+        productName: "pan",
+        price: 610,
+        quanty: 1,
+        img: "/client/media/pan.webp",
+    },
+    {
+        id: 5,
+        productName: "pollo",
+        price: 850,
+        quanty: 1,
+        img: "/client/media/pollo.jpg",
+    },
+    {
+        id: 6,
+        productName: "queso",
+        price: 650,
+        quanty: 1,
+        img: "/client/media/queso.jpg",
+    },
+];
